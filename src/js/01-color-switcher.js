82ab5018f642d81064f43c4a7cb9a2d67d81a613
your_space <katerinakaterina182@gmail.com>
2023-08-26 04:28:17 +0300

    3+

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,22 +1,24 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
-const body = document.querySelector("body");
+const body = document.querySelector('body');
 let timerId = 0;
 
-startBtn.addEventListener("click", () => {
+startBtn.addEventListener('click', () => {
   timerId = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
   startBtn.disabled = true;
 });
 
-stopBtn.addEventListener("click", () => {
+stopBtn.addEventListener('click', () => {
   clearInterval(timerId);
-//   console.log(`stopped`);
-body.style.backgroundColor = '#ffffff';
-startBtn.disabled = false;
+  //   console.log(`stopped`);
+  body.style.backgroundColor = '#ffffff';
+  startBtn.disabled = false;
 });
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
